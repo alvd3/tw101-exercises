cd331bc300a152127bcc1a33f1b459a1e9d7c3a3
alvdd23 <alvdd23@users.noreply.github.com>
2016-07-18 06:03:22 +0200

    Returns the sum of all odd numbers between a given start and end. Exercise 3 complete.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,21 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int oddSum = 0;
+        int startOdd = makeStartOdd(start);
+        for (int i = startOdd; i <end ; i=i+2) {
+            oddSum = oddSum + i;
+        }
+        return oddSum;
     }
+
+    public int makeStartOdd(int start) {
+        int startOdd = start;
+        if (start%2 == 0){
+            startOdd = start + 1;
+        }
+        return startOdd;
+    }
+
+
 }
