1e8b70f37020f8b5f3635994bd4705d19104a4c6
alvdd23 <alvdd23@users.noreply.github.com>
2016-07-18 02:37:28 +0200

    Prints all odd numbers 1 to 100

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,6 +4,8 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
-
+        for (int i = 1; i < 100; i = i+2) {
+            System.out.println(i);
+        }
     }
 }
